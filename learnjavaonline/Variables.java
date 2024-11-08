387cf29cb34f737ecb14b91017f7736c04bfe6fb
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-08 20:40:06 +0530

    Finished variables practice set.

---
+++ learnjavaonline/Variables.java
@@ -0,0 +1,9 @@
+package learnjavaonline;
+
+public class Variables {
+    public static void main(String[] args) {
+        int someNumber;
+        someNumber = 5;
+        System.out.println(someNumber);
+    }
+}
