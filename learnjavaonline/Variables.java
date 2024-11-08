f30d8d6c8c50ef340f4c2c3bb5e9b0f82879cda1
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-08 21:02:33 +0530

    Finished end of lesson exercise.

--- learnjavaonline/Variables.java
+++ learnjavaonline/Variables.java
@@ -2,8 +2,13 @@ package learnjavaonline;
 
 public class Variables {
     public static void main(String[] args) {
-        int someNumber;
-        someNumber = 5;
-        System.out.println(someNumber);
+        char helloWorldCharacter = 'H';
+        short helloWorldShort = 1;
+        int helloWorldInt = 33;
+        byte helloWorldByte = 0;
+        String helloWorldString = "w0r1d";
+        float helloWorldFloat = 2.0f;
+        boolean helloWorldBoolean = true;
+        System.out.println(helloWorldCharacter + "" + helloWorldInt + helloWorldShort + helloWorldByte + " "+ helloWorldString + " " + helloWorldFloat + " " + helloWorldBoolean);
     }
 }
