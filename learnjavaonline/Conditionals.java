45a7e9cd7ecf6d89bb36f36352e6fae621bbe880
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-11-10 09:57:15 +0530

    Conditionals finished.

---
+++ learnjavaonline/Conditionals.java
@@ -0,0 +1,18 @@
+package learnjavaonline;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = "Wow";
+        String c = "Nope!";
+        String d = "Wow!";
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
